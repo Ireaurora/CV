4a8cff753972f7c430af7ad80a62e365690d8fb5
Irene Sarigu <ireaurora@gmail.com>
2020-05-05 08:48:57 +0100

    Update js.js

--- js.js
+++ js.js
@@ -6,6 +6,7 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+// google maps is not offering this feature for free anymore
 function myMap() {
     var mapProp= { center:new google.maps.LatLng(51.508742,-0.120850), zoom:5 };
     var map=new google.maps.Map(document.getElementById("section"),mapProp);
